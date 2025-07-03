a0b9375a002b1eca1be5288df2d906ffbf98a3a0
Programador TI2 <Programador.TI2@PLASTITEC.SA>
2025-07-03 07:55:37 -0500

    Ajuste deply automatico v0.5

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Hero() {
   return (
     <div className="text-center">
-      <h2>Bienvenido al sistema</h2>
+      <h2>Bienvenido</h2>
       <p>Tu aplicación segura con login y registro integrados en modales Bootstrap.</p>
     </div>
   );
